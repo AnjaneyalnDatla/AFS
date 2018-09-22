d2825c9180cb9b7824e77d7a3f0503e94e799125
Kaushik <kaushikgoudgollapalli@Kaushiks-MBP.home>
2018-09-22 01:18:32 -0400

    Changed to dynamic menu

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,22 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 
 declare const $: any;
+
 declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+    children: childArry[];
+}
+declare interface childArry {
     path: string;
     title: string;
     icon: string;
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
-    { path: '/notifications', title: 'Departments',  icon:'layers', class: '' },
-    { path: '/user-profile', title: 'Sales',  icon:'shop_two', class: '' },
-    { path: '/table-list', title: 'Purchases',  icon:'shopping_cart', class: '' },
-    { path: '/typography', title: 'Products and Services',  icon:'content_paste', class: '' },
-    { path: '/icons', title: 'Contacts',  icon:'person', class: '' },
-    { path: '/upgrade', title: 'Accounts',  icon:'account_balance', class: '' },
-    { path: '/maps', title: 'Reports',  icon:'bar_chart', class: '' },
-    { path: '/notifications', title: 'Masters',  icon:'create', class: '' },
+    { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '', children:[] },
+    { path: '/notifications', title: 'Departments',  icon:'layers', class: '', 
+        children:[
+            { path: '/#', title: 'Information Technology',  icon:'layers', class: ''},
+            { path: '/#', title: 'Computer Science',  icon:'layers', class: ''},
+            { path: '/#', title: 'Electronics',  icon:'layers', class: ''},
+            { path: '/#', title: 'Mechanical',  icon:'layers', class: ''},
+    ] },
+    { path: '/user-profile', title: 'Sales',  icon:'shop_two', class: '' , children:[]},
+    { path: '/table-list', title: 'Purchases',  icon:'shopping_cart', class: '', children:[] },
+    { path: '/typography', title: 'Products and Services',  icon:'content_paste', class: '', children:[] },
+    { path: '/icons', title: 'Contacts',  icon:'person', class: '', children:[] },
+    { path: '/upgrade', title: 'Accounts',  icon:'account_balance', class: '', children:[] },
+    { path: '/maps', title: 'Reports',  icon:'bar_chart', class: '', children:[] },
+    { path: '/notifications', title: 'Masters',  icon:'create', class: '' , children:[]},
 ];
 
 @Component({
