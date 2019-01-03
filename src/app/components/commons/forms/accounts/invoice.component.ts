13eae397a9771904ac2c1813c5fa45042d284291
Kaushik Goud Gollapalli <KaushikGollapalli@gmail.com>
2019-01-02 20:26:22 -0500

    Download invoice as pdf

--- src/app/components/commons/forms/accounts/invoice.component.ts
+++ src/app/components/commons/forms/accounts/invoice.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, OnInit, EventEmitter } from '@angular/core';
 import { TransactionsService } from '../../../../_services/transactions.service';
+import * as jspdf from 'jspdf';
+import html2canvas from 'html2canvas';
 
 @Component({
     selector: 'app-invoice',
@@ -8,21 +10,21 @@ import { TransactionsService } from '../../../../_services/transactions.service'
 })
 
 export class InvoiceComponent implements OnInit {
-    
+
     @Input() transactionNumber: any;
     displayData: any;
-    isLoaded:boolean = false;
+    isLoaded: boolean = false;
     subTotal: number = 0;
     total: number = 0;
     tax: number = 0;
-    totalUnitPrice : number = 0;
+    totalUnitPrice: number = 0;
 
     @Output() calculate = new EventEmitter();
     @Output() onBackClick = new EventEmitter();
 
 
-    constructor(private transactionsService: TransactionsService) { 
-       
+    constructor(private transactionsService: TransactionsService) {
+
     }
 
     ngOnInit() {
@@ -32,25 +34,39 @@ export class InvoiceComponent implements OnInit {
     ngOnChanges(changes) {
         console.log(JSON.stringify(this.displayData))
         //this.calculateTotals();
-      }
+    }
 
-    calculateTotals(){
+    calculateTotals() {
         for (let lineItem of this.displayData.lineItems) {
             this.totalUnitPrice = 0;
             this.totalUnitPrice += lineItem.quantity * lineItem.price;
-            this.subTotal += this.totalUnitPrice; 
+            this.subTotal += this.totalUnitPrice;
         }
         this.total = this.subTotal + this.tax;
-            
+
     }
-    onPrint(){
+    onPrint() {
         window.print();
     }
 
-    openPdf(){        
+    openPdf() {
+        var data = document.getElementById('contentToConvert');
+        html2canvas(data).then(canvas => {
+            // Few necessary setting options 
+            var imgWidth = 208;
+            var pageHeight = 295;
+            var imgHeight = canvas.height * imgWidth / canvas.width;
+            var heightLeft = imgHeight;
+
+            const contentDataURL = canvas.toDataURL('image/png')
+            let pdf = new jspdf('p', 'mm', 'a4'); // A4 size page of PDF 
+            var position = 0;
+            pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)
+            pdf.save('invoice.pdf'); // Generated PDF  
+        });
     }
 
-    displayInvoice(val){
+    displayInvoice(val) {
 
     }
 
@@ -59,21 +75,21 @@ export class InvoiceComponent implements OnInit {
         console.log("EVENT");
         console.log(transactionNumber);
         this.transactionsService.getSale(transactionNumber).subscribe(
-          data => {
-            console.log(data);
-            this.displayData = data;
-            console.log(this.displayData);
-            //this.calculateTotals();
-            this.isLoaded = true;
-          }
+            data => {
+                console.log(data);
+                this.displayData = data;
+                console.log(this.displayData);
+                //this.calculateTotals();
+                this.isLoaded = true;
+            }
         );
-      }
+    }
 
-      goBack(){
+    goBack() {
         this.onBackClick.emit();
         //   if(this.displayData.transactionType.name == 'Invoice'){
         //     this.router.navigate(["/sales/create"]);
         //   }
-      }
+    }
 
 }
